680b2ea0024490dabfd0c4442a640500bafdbff3
wangshijun2010 <wangshijun2010@gmail.com>
2017-10-08 17:24:33 +0800

    lesson 01: using error boundary to handle errors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 
 const Profile = ({ user }) => (<div>name: {user.name}</div>);
 
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(err, info) {
+    console.log(err, info);
+    this.setState({ hasError: true });
+    // sendErrorReport(err, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Oops, something went wrong!</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -18,7 +40,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Profile user={this.state.user} />
+        <ErrorBoundary>
+          <Profile user={this.state.user} />
+        </ErrorBoundary>
         <button onClick={this.onClick.bind(this)}>Update</button>
       </div>
     );
