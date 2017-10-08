2726cfbf11318290f56abb13d9e590c000bf5717
wangshijun2010 <wangshijun2010@gmail.com>
2017-10-08 08:29:01 +0800

    prepere for lesson 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
-const Profile = ({ user }) => (<div>username: {user.name}</div>);
+const Profile = ({ user }) => (<div>name: {user.name}</div>);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: { name: 'wangshijun' },
+    };
+  }
+
+  onClick() {
+    this.setState({ user: null });
+  }
 
   render() {
     return (
       <div>
+        <Profile user={this.state.user} />
+        <button onClick={this.onClick.bind(this)}>Update</button>
       </div>
     );
   }
