57f7d833ba3d389f734e77951ae02803fefe42a7
wangshijun2010 <wangshijun2010@gmail.com>
2017-10-08 08:24:24 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const Profile = ({ user }) => (<div>username: {user.name}</div>);
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
