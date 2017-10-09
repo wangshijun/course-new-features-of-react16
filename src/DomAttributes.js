0328cb44c4adba9c3ca432f26ce55b3711948222
wangshijun2010 <wangshijun2010@gmail.com>
2017-10-09 08:11:25 +0800

    lesson 05: dom attributes enhancements

---
+++ src/DomAttributes.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div onClick="alert(1)">Hello</div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './ReactPortals';
+import App from './DomAttributes';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
